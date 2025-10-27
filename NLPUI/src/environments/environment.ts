f6f577320569ba6996dd172fe1eb30c925bc43bc
prabhakat16 <prabhakat16@gmail.com>
2025-10-27 22:26:46 +0530

    progress commit

--- NLPUI/src/environments/environment.ts
+++ NLPUI/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:7195/api',
-  apiTimeout: 90000,
+  apiTimeout: 180000,
   enableDebugMode: true,
   retryAttempts: 2
 };
