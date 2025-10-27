5e58cf13181491df0bd46107cc05f81539883c8e
prabhakat16 <prabhakat16@gmail.com>
2025-10-27 13:37:24 +0530

    Initial check-in

---
+++ NLPUI/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  ngOnInit(): void {
+
+    console.log('App initialized');
+
+  }
+  protected title = 'NLPUI';
+}
